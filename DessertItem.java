108f21cfaa39a62d1f3e7e6cd4c0eb5ea7fd4beb
seuljn <36707871+seul14@users.noreply.github.com>
2023-02-15 08:44:13 -0500

    Update DessertItem.java

--- DessertItem.java
+++ DessertItem.java
@@ -1,18 +1,30 @@
-public abstract class DessertItem 
+public abstract class DessertItem implements Comparable<DessertItem>
 {
 	 private String name;
 	 public DessertItem(String name) 
 	 {
-	   this.name = name;
+		 // takes in the name of the DessertItem
+	 	 this.name = name;
 	 }
+	 
+	 public abstract int getPrice(); 
+	 // abstract method getPrice(), prices are calcualated differently for all items
 
-	 public final String getName() 
-	 {
-	   return name;
+	 public final String getName(){
+		 // returns the name of the DessertItem
+		 return name;
 	 }
-
-	 public abstract int getPrice();
-}
-
-
-	  	
+	 
+	 public int compareTo(DessertItem other){
+		 // Compares prices of current item with desired item
+		 if(this.getPrice() > other.getPrice())
+			 // if current price is numerically larger than the other, returns 1 (positive)
+			 return 1;
+		 else if (this.getPrice() < other.getPrice())
+			 // vice versa, but returns -1 (negative)
+			 return -1;
+		 else 
+			 // if two prices are equal, return 0
+			 return 0;
+	 }
+}  	
