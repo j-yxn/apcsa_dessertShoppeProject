ea424e5f7f788957c0431abc01e6d183231706cc
seuljn <36707871+seul14@users.noreply.github.com>
2023-02-15 08:44:38 -0500

    Update IceCream.java

--- IceCream.java
+++ IceCream.java
@@ -1,15 +1,16 @@
 public class IceCream extends DessertItem 
 {
-	private int price;		
+	private int price; // total price, in cents	
 
 	public IceCream(String name, int p) 
 	{
+		// sets desired name and price
 		super(name);
 		price = p;
 	}
 	
-	public int getPrice() 
-	{
+	public int getPrice(){
+		// retuns total price of the IceCream item
 		return price;
 	}
 }
