4c5e4ad2290aec40b720875e8b083019dc3242bc
ᴅᴀɴɪᴇʟ ʏᴏᴏɴ <36707871+wnsdbs@users.noreply.github.com>
2024-04-29 09:31:01 -0400

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -1,29 +1,27 @@
-public class Candy extends DessertItem 
-{
+public class Candy extends DessertItem {
 	private double weight; // weight
 	private int perPound; // price per pound 	
 	private int price; // total price, in cents
 	
-	public Candy(String name, int p, double w) 
-	{
+	public Candy(String name, int p, double w) {
 		// sets desired name, weight, and price per pound
 		super(name);
 		perPound = p;	
 		weight = w;
 	}
 	
-	public int getPrice(){
+	public int getPrice() {
 		// returns total price of the Candy item
 		price = (int)Math.round(weight * perPound);
 		return price;
 	}
 	
-	public int getPerPound(){
+	public int getPerPound() {
 		// returns the price per pound of the Candy item
 		return perPound;
 	}
 	
-	public double getWeight(){
+	public double getWeight() {
 		// returns the weight of the Candy item
 		return weight;
 	}
