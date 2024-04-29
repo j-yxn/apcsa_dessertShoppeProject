be9564382bcd527a7a1ba9cf8683580d37f090d0
ᴅᴀɴɪᴇʟ ʏᴏᴏɴ <36707871+wnsdbs@users.noreply.github.com>
2024-04-29 09:34:08 -0400

    Update Sundae.java

--- Sundae.java
+++ Sundae.java
@@ -1,23 +1,20 @@
-public class Sundae extends IceCream
-{
+public class Sundae extends IceCream {
 	private int priceTopping; // price for desired topping		
 	private String nameTopping; // name for desired topping		
 	private int price; // total price, in cents
 	
-	public Sundae(String iceCreamName, String toppingName, int iceCreamPrice, int toppingPrice) 
-	{
+	public Sundae(String iceCreamName, String toppingName, int iceCreamPrice, int toppingPrice) {
 		// sets the IceCream name, price, and topping price
 		super(iceCreamName, iceCreamPrice);
 		nameTopping = toppingName;
 		priceTopping = toppingPrice;		
 	}
 	
-	public String getTopName()
-	{
+	public String getTopName() {
 		return nameTopping;
 	}
 	
-	public int getPrice(){
+	public int getPrice() {
 		// returns the total price of the Sundae item
 		price = priceTopping + super.getPrice();
 		return price;
